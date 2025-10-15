45de0aadac8f6ac3ca557d408130f371256aa47c
vivin <vivincirchard@gmail.com>
2025-10-15 08:03:11 +0530

    added JWTAuthenticationScheme in dependencyInjection folder

---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/DepencencyInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.DepencencyInjection
+{
+
+    public static class JwtAuthenticationSchema
+    {
+        public static IServiceCollection AddJwtAuthenticationScheme(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:key").Value!);
+                    string issuer = config.GetSection("Authentication:Issuer").Value!;
+                    string audience = config.GetSection("Authentication:Audience").Value!;
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                    };
+                });
+            return services;
+        }
+    }
+}
--- DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/obj/Debug/net8.0/eCommerce.SharedLibrary.AssemblyInfo.cs
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/obj/Debug/net8.0/eCommerce.SharedLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9add30b118d5f8a8b3073d656abf12b0df3b53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a79475439d9396396ce789b85f016c98f61a47d")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
