ae9add30b118d5f8a8b3073d656abf12b0df3b53
vivin <vivincirchard@gmail.com>
2025-10-15 07:53:27 +0530

    added global exception

---
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,76 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "Sorry, Internal Server Error occured.";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many request made";
+                    statusCode = StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not Authorized to access";
+                    statusCode = StatusCodes.Status401Unauthorized;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "Your are not allowed to access";
+                    statusCode = StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException.LogExceptions(ex);
+
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out of time";
+                    message = "Request timeout... Try again";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+
+                await ModifyHeader(context, title, message, statusCode);
+            }
+        }
+
+        private async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title
+            }), CancellationToken.None);
+            return;
+        }
+    }
+}
--- DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/obj/Debug/net8.0/eCommerce.SharedLibrary.AssemblyInfo.cs
+++ DemoECommerce.SharedLibrarySolution/eCommerce.SharedLibrary/obj/Debug/net8.0/eCommerce.SharedLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f274e7f84b0fb2dccc7c350298084d59308c25b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e712ff752ff2032e03aa659044e08317dedb21")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce.SharedLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
