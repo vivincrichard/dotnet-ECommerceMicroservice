39a5b8d84e5266782ba9343e7f28ccea1dc281c1
vivin <vivincirchard@gmail.com>
2025-10-25 07:27:45 +0530

    added productRepository in Data folder

---
+++ DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Infrastructure.Data
+{
+    public  class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
